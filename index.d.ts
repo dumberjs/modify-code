56f3385cc01c8a65d4e3e95e2e6b348b0514a459
Chunpeng Huo <huocp@me.com>
2019-07-06 20:59:22 +1000

    feat: add TypeScript typing definition

---
+++ index.d.ts
@@ -0,0 +1,30 @@
+export = modifyCode;
+export as namespace modifyCode;
+
+interface ModifyCode {
+  prepend(str: string) : ModifyCode,
+  append(str: string) : ModifyCode,
+  insert(start: number, str: string) : ModifyCode,
+  replace(start: number, end: number, str: string) : ModifyCode,
+  delete(start: number, end: number) : ModifyCode,
+  transform(): ModifyCodeResult
+}
+
+interface ModifyCodeResult {
+  code: string,
+  map: SourceMap
+}
+
+interface SourceMap {
+  version: number,
+  // file, sources, sourcesContent are optional in spec,
+  // but ModifyCodeResult always have them.
+  file: string,
+  sources: string[],
+  sourcesContent: string[],
+  sourceRoot?: string,
+  names?: string[],
+  mappings: string
+}
+
+declare function modifyCode(code: string, filePath?: string): ModifyCode;
