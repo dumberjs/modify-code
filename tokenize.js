e3851fcdbbbdf73529b78ea3ca1c1549fae4070a
Chunpeng Huo <huocp@me.com>
2020-01-28 09:09:32 +1100

    chore: add parser plugin

--- tokenize.js
+++ tokenize.js
@@ -24,6 +24,7 @@ var commonPlugins = [
   'partialApplication',
   // ['pipelineOperator', {proposal: 'minimal'}],
   'throwExpressions',
+  'topLevelAwait'
 ];
 
 module.exports = function(code, options) {
